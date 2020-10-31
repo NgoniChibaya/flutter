4460e1033ef27c56e279589a1a0fdaf8206016c9
ngonichibaya <ngonichibaya@gmail.com>
2020-10-31 03:09:30 +0200

    hy

--- lib/main.dart
+++ lib/main.dart
@@ -3,63 +3,59 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-   
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Name Generator'),
-       ),
-        body: _buildSuggestions(),
-     );
-  }
-
-    Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
-    
-    
-    }
-
-  Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title:Text('Ngonidzaishe Chibaya'),
+      centerTitle: true,
+      backgroundColor: Colors.deepPurple[200],
     ),
-  );
-}
+    body: Row(
+      
+      children: [
+        Expanded(
+           flex: 1,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.red,
+            child: Text('1'),
+
+          ),
+        ),
+         Expanded(
+           flex: 2,
+            child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.green,
+            child: Text('2'),
+
+        ),
+         ),
+
+        Expanded(
+          flex: 3,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.blue,
+            child: Text('3'),
+
+          ),
+        ),
+
+      ],
+
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        print('hello');
+      },
+      child: Text('+'),
+
+    ),
+  )
+));
+
 
   
-}
